87c2e087fed124c98740af55233bd86d16fcb125
dpietrzyk0325 <dpietrzyk5202@conestogac.on.ca>
2023-09-10 14:06:46 -0400

    add: anotherTest project

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is the standard program that they set you up with when you make a new .NET console app
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Wodsdsrld!");
